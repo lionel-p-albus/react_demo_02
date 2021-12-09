02acc28f4161212842fb2d04a4b80174711b96a1
singh_pk <praneed.kla@northbkk.ac.th>
2021-12-09 14:24:47 +0700

    styled components & dynamic props

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,14 +1,4 @@
-import React from 'react';
-
 import styled from "styled-components";
-
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
 
 const Button = styled.button`
   font: inherit;
